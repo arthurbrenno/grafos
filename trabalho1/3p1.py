e999ad8af8e6b91cb94d7edebe3fad036e06afff
Arthur Brenno <64020210+arthurbrenno@users.noreply.github.com>
2025-04-04 21:47:00 -0300

    .

--- trabalho1/3p1.py
+++ trabalho1/3p1.py
@@ -1,43 +1,26 @@
-"""
-Implemente uma classe em Python para representar um grafo n ˜ ao-direcionado utilizando uma lista de ad-
-jac ˆ encias. A classe deve permitir:
-• Calcular tamanho do grafo.
-• Determinar o grau de cada v ´ ertice.
-"""
-
-"""
-A: B
-B: A
-C: B
-
-"""
-
-
 class Grafo:
     def __init__(self):
-        self.grafo = {}
+        self.adjacencias = {}
 
     def add_aresta(self, v1, v2):
-        vertices_do_grafo = self.grafo.keys()
+        vertices_do_grafo = self.adjacencias.keys()
 
         if v1 not in vertices_do_grafo:
-            self.grafo[v1] = set([])
+            self.adjacencias[v1] = set([])
 
         if v2 not in vertices_do_grafo:
-            self.grafo[v2] = set([])
+            self.adjacencias[v2] = set([])
 
-        self.grafo[v1].add(v2)
-        self.grafo[v2].add(v1)
+        self.adjacencias[v1].add(v2)
+        self.adjacencias[v2].add(v1)
 
     def calcular_ordem(self):
-        vertices_do_grafo = self.grafo.keys()
+        vertices_do_grafo = self.adjacencias.keys()
         quantidade_vertices = len(vertices_do_grafo)
         return quantidade_vertices
 
     def calcular_tamanho(self):
-        arestas_grafo = self.grafo.items()
-        quantidade_arestas = len(arestas_grafo)
-        return quantidade_arestas
+        return sum(len(v) for v in self.adjacencias.values()) // 2
 
 
 g = Grafo()
@@ -46,5 +29,6 @@ g.add_aresta("a", "b")
 g.add_aresta("b", "c")
 g.add_aresta("c", "a")
 g.add_aresta("a", "d")
+g.add_aresta("c", "d")
 
 print(g.calcular_tamanho())
